76599b148e1b0ee423fa97783632f9aa06e8f60a
Daniel Morillo <dev-07@mennobot.com>
2025-12-25 22:47:55 -0500

    chore: functions and objects

---
+++ src/14-functions-objects.ts
@@ -0,0 +1,9 @@
+const login = (data: { email: string; password: string }) => {
+  console.log(data.email, data.password);
+};
+
+login({ email: 'dani@mail.com', password: '123' });
+
+const add = (data: { email: string; password: string }) => {
+
+}
